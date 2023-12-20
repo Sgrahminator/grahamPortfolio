2e361746fdb5e06902fa36959d510669a1e19ff3
Sgrahminator <126929663+Sgrahminator@users.noreply.github.com>
2023-12-20 00:07:41 -0700

    update

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Use the MONGODB_URI environment variable (in Heroku) first, otherwise use the local MongoDB URI
-const DB_URI = process.env.MONGODB_URI;
+const DB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/grahamPortfolio";
 
 mongoose.connect(DB_URI,{})
     .then(() => console.log("Established a connection to the database"))
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
+const port = 8000;
 // Require mongoose configuration
 require('./config/mongoose.config');
 
