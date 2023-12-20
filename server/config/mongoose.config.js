ea871b8e05d59c5b2e236564d39f5c6fd6c7d4af
Sgrahminator <126929663+Sgrahminator@users.noreply.github.com>
2023-12-19 22:03:49 -0700

    update

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 // Use the MONGODB_URI environment variable (in Heroku) first, otherwise use the local MongoDB URI
-const DB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/grahamPortfolio";
+const DB_URI = process.env.MONGODB_URI;
 
-mongoose.connect(DB_URI)
+mongoose.connect(DB_URI,{})
     .then(() => console.log("Established a connection to the database"))
     .catch(err => console.log("Something went wrong when connecting to the database", err));
